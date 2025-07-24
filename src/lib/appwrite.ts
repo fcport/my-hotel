3f3d87ef12d8e9555c75c36efa625e5e6bc00a19
Federico <federico.casadei.portfolio@gmail.com>
2025-07-24 13:14:56 +0200

    feature: scaffolngin appwwrite

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,33 @@
+import { Client, Account, Databases } from 'appwrite'
+
+const getEndpoint = () => {
+  console.log(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  return import.meta.env.VITE_APPWRITE_ENDPOINT
+}
+
+const getProjectId = () => {
+  return import.meta.env.VITE_APPWRITE_PROJECT_ID
+}
+
+const getProjectName = () => {
+  return import.meta.env.VITE_APPWRITE_PROJECT_NAME
+}
+
+const client = new Client().setEndpoint(getEndpoint()).setProject(getProjectId())
+
+const account = new Account(client)
+const databases = new Databases(client)
+
+const databaseId = '687bbcbe000afa4a5417'
+const todoCollectionId = '687bbccf00012226d30a'
+
+export {
+  client,
+  account,
+  databases,
+  getEndpoint,
+  getProjectName,
+  getProjectId,
+  databaseId,
+  todoCollectionId,
+}
